01425599640ee6f08f86d5bd288487ff0af83d08
bVd <vandenabeele.bastiaan@gmail.com>
2018-01-12 22:42:14 +0100

    Main program file

---
+++ Main.py
@@ -0,0 +1,7 @@
+from Send_Timesheet_Gsheet import TimeSheetToGsheet
+from Read_Timesheet import ReadTimeSheets
+
+
+if __name__ == "__main__":
+    for key, value in ReadTimeSheets().get_sheet_input().items():
+        TimeSheetToGsheet(key, value)
