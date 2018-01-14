bf1f30bfaa52d28204806f67e4b90cd93b19ec22
bVd <vandenabeele.bastiaan@gmail.com>
2018-01-14 17:10:38 +0100

    main file

--- Main.py
+++ Main.py
@@ -4,4 +4,4 @@ from Read_Timesheet import ReadTimeSheets
 
 if __name__ == "__main__":
     for key, value in ReadTimeSheets().get_sheet_input().items():
-        TimeSheetToGsheet(key, value)
+        TimeSheetToGsheet(key, value).update_summary_day()
