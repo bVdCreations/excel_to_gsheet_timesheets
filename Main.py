7b37db385a876817a066f0746aacfa9331a593b4
bVd <vandenabeele.bastiaan@gmail.com>
2018-01-24 21:58:23 +0100

    restructured Send_Timesheet_Gheet.Py in Super- and SubClasses

--- Main.py
+++ Main.py
@@ -1,7 +1,18 @@
-from Send_Timesheet_Gsheet import TimeSheetToGsheet
+from Send_Timesheet_Gsheet import ToGsheet
 from Read_Timesheet import ReadTimeSheets
 
 
 if __name__ == "__main__":
+    sorted_year = dict()
     for key, value in ReadTimeSheets().get_sheet_input().items():
-        TimeSheetToGsheet(key, value).update_summary_day()
+        if key.split(" ")[2] not in sorted_year.keys():
+            sorted_year[key.split(" ")[2]] = {key:value}
+        else:
+            sorted_year[key.split(" ")[2]].update({key:value})
+        print("key: {}::".format(key))
+        print("     value: {}".format(value))
+        #ToGsheet(key, value).update_summary_day()
+
+
+    for year,timesheets_input in sorted_year.items():
+        ToGsheet(year).update_timesheets(timesheets_input)
--- Send_Timesheet_Gsheet.py
+++ Send_Timesheet_Gsheet.py
@@ -5,22 +5,29 @@ import json
 import datetime
 
 
-class TimeSheetToGsheet:
+class ToGsheet:
 
-    def __init__(self, name: str(), update_input: dict()):
+    def __init__(self, year=2018):
 
         self._scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
         self._creden = ServiceAccountCredentials.from_json_keyfile_name('TimeSheetsToDrive.json', self._scope)
         self._client = gspread.authorize(self._creden)
-        self._time_sheet_input = update_input
-        self._name = name.lower()
-        self._workfile = self.open_spreadsheet()
-        self.update_timesheet()
-        self._day_summary = {'days': {'Monday': 'B', 'Tuesday': 'C', 'Wednesday': 'D', 'Thursday': 'E', 'Friday': 'F',
-                                      'Saturday': 'G', 'Sunday': 'H'},
-                             'extra_info': {'Total': 'J', 'Extra hours': 'K', 'Average': 'L'}}
+        self._year = year
+        self._workfile = self.open_workfile()
+
+    def set_year(self, year:int()):
+        """sets the year of the object"""
+        self._year = year
+
+    def get_year(self):
+        """:return: the year"""
+        return self._year
 
     def get_sheetnames(self):
+        """looks for all the sheets names of the current workfile
+
+        :return: a list with all the sheets names
+        """
         # return a list with the names of all the sheets in the file
         list_names = list()
         for i in range(len(self._workfile.worksheets())):
@@ -28,19 +35,38 @@ class TimeSheetToGsheet:
 
         return list_names
 
-    def open_spreadsheet(self):
+    def open_workfile(self):
+        """open the workfile (spreadsheet) by the set year
+
+        :return: the workfile
+        """
         # open the worksheet based year that is in the excel file
         try:
-            spreadsheet = self._client.open("Timesheets {}".format(self._name.split(' ')[2]))
-            return spreadsheet
+            workfile = self._client.open("Timesheets {}".format(self.get_year()))
+            return workfile
         except:
             print("create the worksheet")
 
-    def create_new_timesheet(self):
+    def update_timesheets(self,input_timesheets: dict()):
+
+        for title_timesheets, values_timesheet in input_timesheets.items():
+            Timesheet(title_timesheets).update_timesheet(values_timesheet)
+
+
+
+
+class Timesheet(ToGsheet):
+
+    def __init__(self, title_timesheet):
+        super().__init__()
+        self._title_timesheet = title_timesheet.lower()
+        self._timesheet = self._open_timesheet()
+
+    def _create_new_timesheet(self):
         # this method creates a new sheet and places the heading for the timesheet
-        if self._name not in self.get_sheetnames():
-            sheet = self._workfile.add_worksheet(self._name, 30, 15)
-            name_split = self._name.split(' ')
+        if self._title_timesheet not in self.get_sheetnames():
+            sheet = self._workfile.add_worksheet(self._title_timesheet, 30, 15)
+            name_split = self._title_timesheet.split(' ')
             cells = {'A1': 'Employee', 'B1': 'Bastiaan Van Denabeele', 'C1': 'Initials', 'D1': 'BVD', 'E1': 'Number',
                      'F1': '51', 'A2': 'Year', 'B2': name_split[2], 'C2': 'Week', 'D2': name_split[1],
                      'A4': 'Type of activity', 'B4': 'Date', 'C4': 'From', 'D4': 'Until', 'E4': 'Project',
@@ -54,37 +80,79 @@ class TimeSheetToGsheet:
         else:
             raise AttributeError("trying to create a sheet that already exists")
 
-    def open_timesheet(self, ):
-        if self._name in self.get_sheetnames():
+    def _open_timesheet(self):
+        """opens or creates the timesheet by the set title in the object
+
+        :return: returns the opened or created sheet
+        """
+        if self._title_timesheet in self.get_sheetnames():
             # open sheet
-            sheet = self._workfile.worksheet(self._name)
+            sheet = self._workfile.worksheet(self._title_timesheet)
         else:
             # create sheet
-            sheet = self.create_new_timesheet()
+            sheet = self._create_new_timesheet()
         return sheet
 
-    def update_timesheet(self):
+    def update_timesheet(self, time_sheet_input: dict()):
+        '''updates every cell of the timesheet with the input dict
+
+        :param time_sheet_input: this is a dict with key as coordinates of a cell
+                                   and the value with the corresponding value of the cell
+        :return: no return
+        '''
         # this puts the readed data from the excel in the rights cell in the sheet in the spreadsheet
-        for key_update, value_update in self._time_sheet_input.items():
-            self.open_timesheet().update_acell(key_update, value_update)
+        for key_update, value_update in time_sheet_input.items():
+            self._open_timesheet().update_acell(key_update, value_update)
 
     def get_last_entry_row_timesheet(self):
+        """looks for the last input in the row
+        (staring from row 5 in col A)
+
+        :return:row number of last input
+        """
         # find the row number of the last entry in the given column
         row = 5
         last_entry_row = row
-        while self.open_timesheet().cell(col=1, row=row).value != '':
+        while self._open_timesheet().cell(col=1, row=row).value != '':
             last_entry_row = row
             row += 1
         return last_entry_row
 
     def get_last_entry_column_timesheet(self, start_column=1):
+        '''looks for the last input in the row
+                (staring from row 5 in col A)
+
+        :return:row number of last input
+        '''
         # find the column number of the last entry the given row
         column = start_column
         last_entry_column = column
-        while self.open_timesheet().cell(col=column, row=4).value != '':
+        while self._open_timesheet().cell(col=column, row=4).value != '':
             last_entry_column = column
             column += 1
         return last_entry_column
+
+    def title(self):
+        """:return title of timesheet"""
+        return self._title_timesheet
+
+    def cell(self, row: int(), col: int()):
+        """get the cell of the timesheet
+
+        :param row: the row position of the cell
+        :param col: the column position of the cell
+        :return: the specific object cell
+        """
+        return self._timesheet.cell(row=row, col=col)
+
+
+class DaySummary(ToGsheet):
+
+    def __init__(self):
+        super().__init__()
+        self._day_summary = {'days': {'Monday': 'B', 'Tuesday': 'C', 'Wednesday': 'D', 'Thursday': 'E', 'Friday': 'F',
+                                      'Saturday': 'G', 'Sunday': 'H'},
+                             'extra_info': {'Total': 'J', 'Extra hours': 'K', 'Average': 'L'}}
 
     def open_day_summary(self):
 
@@ -110,23 +178,36 @@ class TimeSheetToGsheet:
         else:
             raise AttributeError("trying to create a sheet that already exists")
 
-    def update_summary_day(self):
+    def update_summary_week(self, week: "number of the week"):
+        """updates the summary formulas in the sheet 'day_summary' in a given week
+
+        :param week: the number of the week
+        :param year: the year
+        :return:
+        """
         sheet_day_summary = self.open_day_summary()
-        for key_update, value_update in self.create_summary_formulas().items():
+        for key_update, value_update in \
+                self.create_summary_formulas(week).items():
             sheet_day_summary.update_acell(key_update, value_update)
 
-    def create_summary_formulas(self):
+    def create_summary_formulas(self, week: "number of the week"):
+        """greates a dict with the gsheets formula's for the spefic week
+
+        :param week: the number of the week
+        :param year: the year
+        :return: dict with the formula's
+        """
 
         summary_formulas = dict()
-
+        timesheet = Timesheet("week {} {}".format(week, self.get_year()))
+        timesheet_title = timesheet.title()
         type_of_activity = self.load_type_of_acticity()
-        timesheet = self.open_timesheet()
 
-        number_week = self._name.split(' ')[1]
+        number_week = str(timesheet_title).split(' ')[1]
         column_position = str(int(number_week) + 1)
 
         # loop true al input value rows in timesheet
-        for row in range(5, self.get_last_entry_row_timesheet()+1):
+        for row in range(5, timesheet.get_last_entry_row_timesheet()+1):
             # define the cell coordinates in the sheet day summary
             day = self.day_of_week(timesheet.cell(row=row, col=2).value)
             key_formula = self._day_summary.get("days").get(day) + column_position
@@ -137,13 +218,13 @@ class TimeSheetToGsheet:
                 if key_formula in summary_formulas.keys():
                     # update the formula
                     oldform = summary_formulas[key_formula]
-                    newform = oldform[:-4] + "+({}-{}) ".format("'"+self._name + "'!D" + str(row),
-                                                                "'" + self._name + "'!C" + str(row)) + ")*24"
+                    newform = oldform[:-4] + "+({}-{}) ".format("'"+timesheet_title + "'!D" + str(row),
+                                                                "'" + timesheet_title + "'!C" + str(row)) + ")*24"
                     summary_formulas[key_formula] = newform
                 else:
                     # create the formula
-                    summary_formulas[key_formula] = "=(({}-{}) )*24".format("'"+self._name + "'!D" + str(row),
-                                                                            "'" + self._name + "'!C" + str(row))
+                    summary_formulas[key_formula] = "=(({}-{}) )*24".format("'"+timesheet_title + "'!D" + str(row),
+                                                                            "'" + timesheet_title + "'!C" + str(row))
             elif type_of_activity.get(timesheet.cell(row=row, col=1).value) == "Day Off":
                 if timesheet.cell(row=row, col=1).value == "Day off (special reason, describe in comments)":
                     summary_formulas[key_formula] = "read the comment for more info"
@@ -157,17 +238,21 @@ class TimeSheetToGsheet:
             "=SUM(B{}:H{})".format(column_position, column_position)
         summary_formulas[self._day_summary.get('extra_info').get('Average') + column_position] = \
             "=AVERAGE(B{}:H{})".format(column_position, column_position)
-        summary_formulas['A'+column_position] = self._name
+        summary_formulas['A'+column_position] = timesheet_title
 
         return summary_formulas
 
     @staticmethod
-    def day_of_week(time):
+    def day_of_week(time: '%Y-%m-%d %H:%M:%S'):
+        """
+        :return: the day of the week from the variable time gives as '%Y-%m-%d %H:%M:%S'
+        """
         dayoftheweek = datetime.datetime.strptime(time, '%Y-%m-%d %H:%M:%S').strftime('%A')
         return dayoftheweek
 
     @staticmethod
     def load_type_of_acticity():
+        """:return: dict of types of activity in the company from a json file"""
         with open("type_of_activity.json") as json_data:
             type_of_activity = json.load(json_data)
         return type_of_activity
