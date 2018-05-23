88e311eb2181ea1b54bc9311e96ff6fdd7a3f980
bVdCreations <vandenabeele.bastiaan@gmail.com>
2018-05-23 15:50:49 +0200

    added print statement for the terminal

--- Main.py
+++ Main.py
@@ -4,13 +4,17 @@ from Send_Timesheet_Gsheet import DaySummary
 
 
 if __name__ == "__main__":
-
+    
+    print('started')
     for year, timesheets_input in ReadTimeSheets().get_sheet_input().items():
+        
         ToGsheet(year).update_timesheets(timesheets_input)
+        print('updated the sheet {} to gheet {}'.format(timesheets_input.keys() ,year))
         for key in timesheets_input.keys():
             try:
                 number = int(key.split(' ')[1])
                 DaySummary().update_summary_week(number)
+                print('update summary week for week {}'.format(number))
             except ValueError:
                 raise ValueError('can not confert {} to a number'.format(key))
 
